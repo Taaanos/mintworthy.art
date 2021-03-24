6963a11eb38dde14863b0f354f7133b1f255ff55
Thanos Amoutzias <ktmfuji@gmail.com>
2021-03-24 23:03:35 +0100

    Read from .csv

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import utils
 import os
 import uuid
 from web3.auto.infura import w3
+import numpy as np
 
 glitcher = ImageGlitcher()
 
 assets_dir = "assets/"
 extension = ".png"
+font = "FredokaOne-Regular.ttf"
 
 
 # construct_path constructs the path for the assets.
@@ -34,7 +36,7 @@ def make_opaque(on_image, factor):
 # resize an image by a factor
 # e.g to make it 40% of the orinal apply a factor of 0.4
 def scale_image(on_image, factor):
-    new_size = (int(on_image.size[0] * factor), int(on_image.size[1] * factor))
+    new_size = (int(on_image.width * factor), int(on_image.height * factor))
     return on_image.resize(new_size)
 
 
@@ -42,8 +44,8 @@ def scale_image(on_image, factor):
 def rnd_coordinates():
     # edge factor regulates how close the layer is applied to the border
     edge_factor = 0.8
-    x = randint(0, int(bg.size[0] * edge_factor))
-    y = randint(0, int(bg.size[1] * edge_factor))
+    x = randint(0, int(bg.width * edge_factor))
+    y = randint(0, int(bg.height * edge_factor))
     return [x, y]
 
 
@@ -52,17 +54,17 @@ def rnd_coordinates():
 # the coordinates
 def draw_words(str_to_draw, layer, size, color, x, y):
     draw = ImageDraw.Draw(layer)
-    font = ImageFont.truetype("FredokaOne-Regular.ttf", size)
-    draw.text((x, y), str_to_draw, "#" + color, font=font)
+    f = ImageFont.truetype(font, size)
+    draw.text((x, y), str_to_draw, "#" + color, font=f)
 
 
 # draw_multiline draws a string and respects the newlines
 def draw_multiline(str_to_draw, layer, size, x, y):
     draw = ImageDraw.Draw(layer)
-    font = ImageFont.truetype("FredokaOne-Regular.ttf", size)
+    f = ImageFont.truetype(font, size)
     draw.multiline_text((x, y),
                         str_to_draw,
-                        font=font,
+                        font=f,
                         fill=(256, 256, 256),
                         spacing=100)
 
@@ -72,8 +74,8 @@ def draw_circle(layer, x, y):
     draw = ImageDraw.Draw(layer)
     circle_radius_factor = 0.6
     draw.pieslice([(x, y),
-                   (x + layer.size[0] * circle_radius_factor,
-                    y + layer.size[0] * circle_radius_factor)],
+                   (x + layer.width * circle_radius_factor,
+                    y + layer.width * circle_radius_factor)],
                   0,
                   360,
                   fill='#' + colors[randint(0,
@@ -87,8 +89,8 @@ def draw_circle(layer, x, y):
 # if you want to place an item on the bottom right corner
 # the percentages would be ~ 0.70, 0.90
 def place_coord(on_layer, x_perc, y_perc):
-    x = int(on_layer.size[0] * x_perc)
-    y = int(on_layer.size[1] * y_perc)
+    x = int(on_layer.width * x_perc)
+    y = int(on_layer.height * y_perc)
     return [x, y]
 
 
@@ -108,14 +110,10 @@ signature = Image.open("assets/signature.png")
 logo = Image.open("assets/logo.png")
 
 # Create image to be used as layer0 to become the borders of the final canvas
-border = Image.new('RGB', (int(bg.size[0] * 1.1), int(bg.size[1] * 1.1)),
+border = Image.new(bg.mode, (int(bg.width * 1.1), int(bg.height * 1.1)),
                    color='white')
-
-colors = [
-    'a1cae2', 'c2b092', 'cfc5a5', 'eae3c8', 'ffaec0', 'ffd384', 'ffab73',
-    'e4bad4', 'f6dfeb', 'caf7e3'
-]
-colored_blank_image = Image.new('RGB', (int(bg.size[0]), int(bg.size[1])),
+colors = np.genfromtxt('colors.csv', dtype=str)
+colored_blank_image = Image.new(bg.mode, (int(bg.width), int(bg.height)),
                                 color='#' + colors[randint(0,
                                                            len(colors) - 1)])
 bg = colored_blank_image
@@ -125,24 +123,16 @@ draw_circle(bg, x_circle, y_circle)
 bg.paste(insect, (rnd_coordinates()), insect.convert("RGBA"))
 insect = Image.open(construct_path(2))
 bg.paste(insect, (rnd_coordinates()), insect.convert("RGBA"))
-bg = glitcher.glitch_image(bg, 8, color_offset=True)
+bg = glitcher.glitch_image(bg, randint(1, 8), color_offset=True)
 bg.paste(plant, (rnd_coordinates()), plant.convert("RGBA"))
 
 # draw .word
-words = [
-    '.visualise', '.imagine', '.think', '.empower', '.explore', '.engage',
-    '.motivate', '.inspire', '.aspire', '.belong', '.accept', '.defy',
-    '.exist', '.transcend', '.catalyse', '.overcome', '.dismantle',
-    '.meditate', '.reflect', '.express'
-]
+words = np.genfromtxt('words.csv', dtype=str)
 x, y = place_coord(bg, 0.05, 0.9)
-draw_words(words[randint(0, len(words) - 1)], bg, 256, "ffffff", x, y)
+draw_words('.' + words[randint(0, len(words) - 1)], bg, 256, "ffffff", x, y)
 
 # draw one letter
-letters = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
+letters = np.genfromtxt('letters.csv', dtype=str)
 x_letter, y_letter = place_coord(bg, 0.75, 0.02)
 draw_words(letters[randint(0,
                            len(letters) - 1)], bg, 512, "ffffff", x_letter,
@@ -167,7 +157,7 @@ phrases = [
 x, y = place_coord(bg, 0.1, 0.1)
 draw_multiline(phrases[randint(0, len(phrases) - 1)], bg, 128, x, y_letter * 2)
 
-border.paste(bg, (int(border.size[0] * 0.045), int(border.size[1] * 0.035)),
+border.paste(bg, (int(border.width * 0.045), int(border.height * 0.035)),
              bg.convert("RGBA"))
 
 signature = scale_image(signature, 0.4)
@@ -179,7 +169,7 @@ border.paste(logo, place_coord(border, 0.68, 0.943), logo.convert("RGBA"))
 # draw hash
 x, y = place_coord(border, 0.045, 0.96)
 draw_words(get_block_hash(), border, 32, "bbbbbb", x, y)
-# border.show()
+border.show()
 
 # reduce the generated image size. Reduce is not compress.
 # Reduce touches the resolution.
